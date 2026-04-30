d61c05479adbe8721fe7fffc0c652954c2c9e191
NDiMT <100854436+NDiMT@users.noreply.github.com>
2026-05-01 02:27:54 +0300

    Add Android app build config

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ndimt.pixelaspire"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ndimt.pixelaspire"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
